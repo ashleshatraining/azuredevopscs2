b81d1700bd67c1b5f010c8392e7bb617fae330e4
ashleshatraining <56999228+ashleshatraining@users.noreply.github.com>
2025-08-16 16:43:40 +0530

    Update main.tf

--- TF-files/main.tf
+++ TF-files/main.tf
@@ -122,7 +122,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   location              = data.azurerm_resource_group.rg.location
   resource_group_name   = data.azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = "Standard_B1s"
+  size                  = "Standard_B2s"
 
   os_disk {
     name                 = "myOsDisk"
