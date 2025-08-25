99480ddfcc285808c8fbcdc9e63b8f6525572751
ashleshatraining <56999228+ashleshatraining@users.noreply.github.com>
2025-08-25 11:59:50 +0000

    Update main.tf

--- TF-files/main.tf
+++ TF-files/main.tf
@@ -150,24 +150,7 @@ resource "azurerm_storage_account" "storage" {
   }
 }
 
-resource "azurerm_mssql_server" "sql-server" {
-  name                         = "${var.prefix}server7"
-  resource_group_name          = data.azurerm_resource_group.rg.name
-  location                     = data.azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "${var.SLOGIN}"
-  administrator_login_password = "admin@12345$"
-}
 
-resource "azurerm_mssql_database" "db" {
-  name           = "${var.prefix}db"
-  server_id      = azurerm_mssql_server.sql-server.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  max_size_gb    = 2
-  read_scale     = false
-  sku_name       = "S0"
-  zone_redundant = false
 
   tags = {
     foo = "bar"
